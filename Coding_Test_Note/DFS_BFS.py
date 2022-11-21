7d4c66b892d5c5dcf78e5c614d18579b168f3656
Brother_gyu <jang07133@gmail.com>
2022-11-21 18:48:36 +0900

    -

---
+++ Coding_Test_Note/DFS_BFS.py
@@ -0,0 +1,39 @@
+def dfs(graph, v, visited):
+    visited[v]=True
+    print(v, end=' ')
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+from collections import deque
+def bfs(graph, start, visited):
+    queue=deque([start])
+    visited[start]=True
+    while queue:
+        v=queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] =True
+
+if __name__ == '__main__':
+    graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+    ]
+    visited = [False]*9
+    print('DFS')
+    dfs(graph, 1, visited)
+    print('\nBFS')
+    visited = [False]*9
+    bfs(graph, 1, visited)
